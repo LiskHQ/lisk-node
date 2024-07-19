28837d3ce7c037945678f687851033ccf37eec84
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2024-07-19 15:12:30 +0200

    :hammer: Add retry mechanism for snapshot download & integrity verification

--- geth/download-apply-snapshot.sh
+++ geth/download-apply-snapshot.sh
@@ -5,7 +5,7 @@ set -eu
 [[ $(type -t echoBanner) == function ]] || alias echoBanner=echo
 
 # Translate APPLY_SNAPSHOT to uppercase; default to FALSE
-APPLY_SNAPSHOT=$(echo "${APPLY_SNAPSHOT-false}" | tr "[:lower:]" "[:upper:]")
+readonly APPLY_SNAPSHOT=$(echo "${APPLY_SNAPSHOT:-false}" | tr "[:lower:]" "[:upper:]")
 
 if [[ "$APPLY_SNAPSHOT" == "FALSE" ]]; then
   echo "Automatic snapshot application disabled; to enable, set 'APPLY_SNAPSHOT=true' and restart"
@@ -22,39 +22,50 @@ if [[ "${GETH_DATA_DIR-x}" == x ]]; then
     exit 2
 fi
 
-SNAPSHOT_DIR=./snapshot
-SNAPSHOT_FILENAME=$(basename ${SNAPSHOT_URL})
-SNAPSHOT_SHA256_URL="${SNAPSHOT_URL}.SHA256"
-SNAPSHOT_SHA256_FILENAME="${SNAPSHOT_FILENAME}.SHA256"
+readonly SNAPSHOT_DIR=./snapshot
+readonly SNAPSHOT_FILENAME=$(basename ${SNAPSHOT_URL})
+readonly SNAPSHOT_SHA256_URL="${SNAPSHOT_URL}.SHA256"
+readonly SNAPSHOT_SHA256_FILENAME="${SNAPSHOT_FILENAME}.SHA256"
+readonly SNAPSHOT_DOWNLOAD_MAX_TRIES=3
 
 # Clear any existing snapshots
 rm -rf $SNAPSHOT_DIR
 
 # Download the snapshot & the checksum file
 echoBanner "Downloading snapshot to '${SNAPSHOT_DIR}/$SNAPSHOT_FILENAME' from '${SNAPSHOT_URL}'..."
-curl --create-dirs --output $SNAPSHOT_DIR/$SNAPSHOT_FILENAME --location $SNAPSHOT_URL
-curl --create-dirs --output $SNAPSHOT_DIR/$SNAPSHOT_SHA256_FILENAME --location $SNAPSHOT_SHA256_URL
+num_tries_left=$SNAPSHOT_DOWNLOAD_MAX_TRIES
+download_and_verify(){
+  echo -e "Number of tries left: ${num_tries_left}"
+  : $((--num_tries_left)) # Reduce num_tries_left
 
-echoBanner "Verifying integrity of the downloaded snapshot..."
-if command -v sha256sum &>/dev/null; then
-  (cd $SNAPSHOT_DIR && sha256sum --check $SNAPSHOT_SHA256_FILENAME &>/dev/null)
-elif command -v shasum &>/dev/null; then
-  (cd $SNAPSHOT_DIR && shasum --algorithm 256 --check $SNAPSHOT_SHA256_FILENAME &>/dev/null)
-else
-  echo "Neither sha256sum nor shasum available. Skipping..."
-  exit 9
-fi
+  echo -e "\nDownloading snapshot..."
+  curl --create-dirs --output $SNAPSHOT_DIR/$SNAPSHOT_FILENAME --location $SNAPSHOT_URL
 
-if [[ "$?" == "0" ]]; then
+  echo -e "\nDownloading snapshot checksum..."
+  curl --create-dirs --output $SNAPSHOT_DIR/$SNAPSHOT_SHA256_FILENAME --location $SNAPSHOT_SHA256_URL
+
+  echo -e "\nVerifying integrity of the downloaded snapshot..."
+  if command -v sha256sum &>/dev/null; then
+    (cd $SNAPSHOT_DIR && sha256sum --check $SNAPSHOT_SHA256_FILENAME &>/dev/null)
+  elif command -v shasum &>/dev/null; then
+    (cd $SNAPSHOT_DIR && shasum --algorithm 256 --check $SNAPSHOT_SHA256_FILENAME &>/dev/null)
+  else
+    echo "Neither sha256sum nor shasum available. Skipping..."
+    return 9
+  fi
+
+  if [[ "$?" != "0" ]]; then
+    echo "Snapshot is corrupted. Skipping snapshot application..."
+    return 10
+  fi
+
   echo "Snapshot successfully downloaded and verified"
-else
-  echo "Snapshot is corrupted. Skipping snapshot application..."
-  exit 10
-fi
+}
+for i in $(seq 1 $SNAPSHOT_DOWNLOAD_MAX_TRIES); do download_and_verify && returncode=0 && break || returncode=$? && sleep 10; done; (exit $returncode)
 
 # Import snapshot
 echoBanner "Importing snapshot..."
-./geth import --datadir=$GETH_DATA_DIR $SNAPSHOT_DIR/$SNAPSHOT_FILENAME
+./geth import --syncmode "${OP_GETH_SYNCMODE:-full}" --datadir=$GETH_DATA_DIR $SNAPSHOT_DIR/$SNAPSHOT_FILENAME
 if [[ "$?" == "0" ]]; then
   echo "Snapshot successfully imported"
 else
