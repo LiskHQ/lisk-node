52260a96d1a1cc7b9b1df05aa354072cd4bd432e
Du, Chengbin <robertduu@gmail.com>
2025-10-13 11:31:07 +0800

    fix url

--- reth/download-apply-snapshot.sh
+++ reth/download-apply-snapshot.sh
@@ -30,7 +30,7 @@ fi
 
 # Snapshot base URLs
 readonly SNAPSHOT_BASE_URL_DEFAULT="https://snapshots.lisk.com"
-readonly SNAPSHOT_BASE_URL_ALTERNATE="https://s3.fr-par.scw.cloud/snapshots.lisk.com/"
+readonly SNAPSHOT_BASE_URL_ALTERNATE="https://s3.fr-par.scw.cloud/snapshots.lisk.com"
 
 # Automatically resolve SNAPSHOT_URL, if not specified
 SNAPSHOT_URL="$SNAPSHOT_URL"
